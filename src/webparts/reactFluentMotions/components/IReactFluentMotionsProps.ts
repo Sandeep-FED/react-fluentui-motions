68feb903a130a82ac26294ee326d17f286f8e93f
Sandeep-FED <sandymankara11@gmail.com>
2025-01-01 22:08:43 +0530

    bring up solution

---
+++ src/webparts/reactFluentMotions/components/IReactFluentMotionsProps.ts
@@ -0,0 +1,7 @@
+export interface IReactFluentMotionsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
